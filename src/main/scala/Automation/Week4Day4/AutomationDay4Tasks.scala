3435a1d863fdac8a3bc0f7ee393bd7885b359956
Adam Chery <adam.chery@Adams-MacBook-Air.local>
2025-06-05 17:49:04 +0100

    HTML MVP Started

--- src/main/scala/Automation/Week4Day4/AutomationDay4Tasks.scala
+++ src/main/scala/Automation/Week4Day4/AutomationDay4Tasks.scala
@@ -7,44 +7,92 @@ import java.util
 
 object AutomationDay4Tasks extends App {
 
-  val driver: WebDriver = new ChromeDriver()
-
-  driver.get("https://www.selenium.dev/selenium/web/web-form.html")
+  //PPT MVP
+  println("PPT MVP: ")
+  val formDriver: WebDriver = new ChromeDriver()
+  formDriver.get("https://www.selenium.dev/selenium/web/web-form.html")
 
   //By ID
-  val firstName: WebElement = driver.findElement(By.id("my-text-id"))
+  val firstName: WebElement = formDriver.findElement(By.id("my-text-id"))
   firstName.sendKeys("Adam")
   println("Name Input - Passed")
 
   //By Name
-  val password: WebElement = driver.findElement(By.name("my-password"))
+  val password: WebElement = formDriver.findElement(By.name("my-password"))
   password.sendKeys("Password123")
   println("Password Input - Passed")
 
   //By Class Name
-  val classNameSearch: util.List[WebElement] = driver.findElements(By.className("form-control"))
+  val classNameSearch: util.List[WebElement] = formDriver.findElements(By.className("form-control"))
   val comment: WebElement = classNameSearch.get(2)
   comment.sendKeys("Test Comment")
   println("Text Area Input - Passed")
 
   //By Tag Name
-  val tagCount: util.List[WebElement] = driver.findElements(By.tagName("input"))
+  val tagCount: util.List[WebElement] = formDriver.findElements(By.tagName("input"))
   val inputCount: Int = tagCount.size()
   println(s"Total number of input tags: $inputCount - Passed")
 
   //By Link Text
-  val returnIndex: WebElement = driver.findElement(By.linkText("Return to index"))
+  val returnIndex: WebElement = formDriver.findElement(By.linkText("Return to index"))
   returnIndex.click()
   println("Link Text Click - Passed")
 
   //By Partial Link Text
-  driver.navigate().back()
+  formDriver.navigate().back()
   println("Return to Webpage - Passed")
-  val returnPartial: WebElement = driver.findElement(By.partialLinkText("Return"))
+  val returnPartial: WebElement = formDriver.findElement(By.partialLinkText("Return"))
   returnPartial.click()
   println("Return to Index (Partial) - Passed")
 
 
-  driver.close()
+  formDriver.close()
+
+
+  //HTML MVP
+  println("HTML MVP 1")
+  val w3driver: WebDriver = new ChromeDriver()
+  w3driver.get("https://www.w3schools.com/html/html_examples.asp")
+
+  println("MVP 1a:")
+  val buttonElements: util.List[WebElement] = w3driver.findElements(By.className("w3-button"))
+  println(s"There is a total of " + buttonElements.size() + " button elements")
+
+  println("MVP 1b:")
+  val imgElements: util.List[WebElement] = w3driver.findElements(By.tagName("img"))
+  println(s"There is a total of " + imgElements.size() + " image elements")
+
+  println("MVP 1c:")
+  val elementCounta: util.List[WebElement] = w3driver.findElements(By.tagName("a"))
+  val elementCounth1: util.List[WebElement] = w3driver.findElements(By.tagName("h1"))
+  val aCount: Int = elementCounta.size()
+  val hCount: Int = elementCounth1.size()
+  println(s"There is $aCount <a> tags on this page")
+  println(s"There is $hCount <h1> tags on this page")
+
+  println("MVP 1d:")
+
+  println("MVP 1e:")
+
+  w3driver.close()
+
+  println("HTML MVP 2")
+  val demoDriver: WebDriver = new ChromeDriver()
+  demoDriver.get("https://demoqa.com/")
+
+  println("MVP 2a:")
+  demoDriver.navigate().to("https://demoqa.com/text-box")
+  val fullName: WebElement = demoDriver.findElement(By.xpath("//input[@placeholder = 'Full Name']"))
+  fullName.sendKeys("Adam Chery")
+  println("Full Name navigate and locate - Passed")
+
+  println("MVP 2b:")
+  demoDriver.navigate().to("https://demoqa.com/text-box")
+
+
+
+
+
+ demoDriver.close()
 
 }
