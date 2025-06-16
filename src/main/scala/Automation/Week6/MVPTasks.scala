f8841de06f8f100c050ba3ee98a863301cfa65ae
Adam Chery <adam.chery@Adams-MacBook-Air.local>
2025-06-16 15:31:05 +0100

    mvp tasks added

---
+++ src/main/scala/Automation/Week6/MVPTasks.scala
@@ -0,0 +1,60 @@
+package Automation.Week6
+
+import Automation.Week6.ScreenCapture.driver
+import org.openqa.selenium.chrome.{ChromeDriver, ChromeOptions}
+import org.openqa.selenium.io.FileHandler
+import org.openqa.selenium.{By, NoSuchElementException, OutputType, TakesScreenshot, TimeoutException, WebDriver, WebElement}
+
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Date
+
+object MVPTasks extends App {
+
+  val options = new ChromeOptions
+  options.addArguments("--headless=new")
+
+  val driver: WebDriver = new ChromeDriver(options)
+
+  def takeScreenCapture(driver: WebDriver): Unit  = {
+    if(driver.getCurrentUrl == "https://the-internet.herokuapp.com/secure"){
+      val timeStamp = new SimpleDateFormat("HHmmss").format(new Date())
+      val dateStamp = new SimpleDateFormat("yyyy-MM-dd").format(new Date())
+      val srcTimestamp: File = driver.asInstanceOf[TakesScreenshot].getScreenshotAs(OutputType.FILE)
+      FileHandler.copy(srcTimestamp, new File(s"/Users/adam.chery/Documents/screenshots/${dateStamp}-success-$timeStamp.png"))
+      println("Success")
+    }else{
+      val timeStamp = new SimpleDateFormat("HHmmss").format(new Date())
+      val dateStamp = new SimpleDateFormat("yyyy-MM-dd").format(new Date())
+      val srcTimestamp: File = driver.asInstanceOf[TakesScreenshot].getScreenshotAs(OutputType.FILE)
+      FileHandler.copy(srcTimestamp, new File(s"/Users/adam.chery/Documents/screenshots/${dateStamp}-failure-$timeStamp.png"))
+      println("Failure")
+    }
+  }
+
+  try{
+    driver.get("https://the-internet.herokuapp.com/login")
+
+    val username: WebElement = driver.findElement(By.id("username"))
+    val password: WebElement = driver.findElement(By.id("password"))
+    val login: WebElement = driver.findElement(By.className("radius"))
+
+    username.sendKeys("tomsmith")
+    password.sendKeys("SuperSecretPassword!")
+    login.click()
+
+    takeScreenCapture(driver)
+
+  }catch {
+    case e: NoSuchElementException =>
+      println("No Element Found")
+
+    case e: TimeoutException =>
+      println("Page Timeout")
+  }finally {
+    if(driver != null){
+      driver.quit()
+    }
+  }
+
+}
